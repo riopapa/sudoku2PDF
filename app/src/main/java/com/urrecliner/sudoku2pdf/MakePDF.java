61526c28456bdb67a1010c282179fcde2030e9be
Woncherl Ha (하원철) <riopapa@gmail.com>
2022-12-19 09:43:51 +0900

    Change number font, align formats

--- app/src/main/java/com/urrecliner/sudoku2pdf/MakePDF.java
+++ app/src/main/java/com/urrecliner/sudoku2pdf/MakePDF.java
@@ -24,13 +24,8 @@ class MakePDF {
 
     static void createPDF(String [] blankTables, String [] answerTables, String [] commentTables) {
 
-//        int [][] xyTable = new int[9][9];
-        String downLoadFolder = Environment.getExternalStorageDirectory().getPath() + "/download";
-//        File file = new File(downLoadFolder);
-//        if (!file.exists()) {
-//            Log.w("file", file+" created");
-//            file.mkdirs();
-//        }
+        String downLoadFolder = Environment.getExternalStorageDirectory().getPath();
+
         Bitmap sigMap = BitmapFactory.decodeResource(mContext.getResources(), R.mipmap.my_sign_yellow);
         int xSig = sigMap.getWidth() / 5;
         int ySig = sigMap.getHeight() / 5;
@@ -63,14 +58,16 @@ class MakePDF {
         Paint pNumb = new Paint();
         pNumb.setColor(Color.BLUE);
         pNumb.setStrokeWidth(1);
-        pNumb.setTypeface(ResourcesCompat.getFont(mContext, R.font.hj_hanseo_a));
+        pNumb.setTypeface(ResourcesCompat.getFont(mContext, R.font.radioland_regular));
         pNumb.setTextSize(36);
+        pNumb.setTextAlign(Paint.Align.CENTER);
         pNumb.setStyle(Paint.Style.FILL_AND_STROKE);
 
         Paint pMemo = new Paint();
         pMemo.setColor(Color.BLUE);
         pMemo.setStyle(Paint.Style.STROKE);
         pMemo.setStrokeWidth(0);
+        pMemo.setTextAlign(Paint.Align.CENTER);
         pMemo.setStyle(Paint.Style.FILL_AND_STROKE);
         pMemo.setTextSize(32);
 
@@ -78,6 +75,7 @@ class MakePDF {
         pSig.setColor(Color.GREEN);
         pSig.setStyle(Paint.Style.STROKE);
         pSig.setStrokeWidth(0);
+        pSig.setAlpha(128);
         pSig.setStyle(Paint.Style.FILL_AND_STROKE);
         pSig.setTextSize(16);
 
@@ -100,13 +98,15 @@ class MakePDF {
             }
             int xBase = space + 10;
             int yBase = space + (idx % 2) * boxWidth * 9 + (idx % 2) * boxWidth / 2 + 40;
+            int xGap = boxWidth/2;
+            int yGap = boxWidth/2+8;
             for (int row = 0; row < 9; row++) {
                 for (int col = 0; col < 9; col++) {
                     int xPos = xBase + col * boxWidth;
                     int yPos = yBase + row * boxWidth;
                     canvas.drawRect(xPos, yPos, xPos + boxWidth, yPos + boxWidth, pRect);
                     if (xyTable[row][col] > 0) {
-                        canvas.drawText("" + xyTable[row][col], xPos + boxWidth/2f -4, yPos + space + boxWidth/2f, pNumb);
+                        canvas.drawText("" + xyTable[row][col], xPos + xGap, yPos + space + yGap, pNumb);
                     } else {
                         canvas.drawLine(xPos + boxWidth3, yPos, xPos + boxWidth3, yPos + boxWidth, pDotted);
                         canvas.drawLine(xPos + boxWidth3 + boxWidth3, yPos,
@@ -125,7 +125,7 @@ class MakePDF {
                     int yPos = yBase + row * boxWidth;
                     canvas.drawRect(xBase, yBase, xPos + boxWidth*3, yPos + boxWidth*3, pRectO);
                 }
-            canvas.drawText("("+idx+") "+commentTables[idx], xBase + 12 + boxWidth*9, yBase + boxWidth/2f, pMemo);
+            canvas.drawText("("+idx+") "+commentTables[idx], xBase + 24 + boxWidth*9, yBase + boxWidth/2f, pMemo);
         }
         printSignature(sigMap, xSig, ySig, pgWidth, pgHeight, canvas, pSig);
 
@@ -155,16 +155,16 @@ class MakePDF {
 
         boxWidth = pgWidth / 4 / 10;    // 4 answer for 1 line, maybe 26
 
-        pNumb.setTextSize(boxWidth/2f+4); // pNumb : given number
-        pMemo.setTextSize(boxWidth/2f);  // pMemo : answer
+        pNumb.setTextSize(boxWidth/2f); // pNumb : answer number
+        pMemo.setTextSize(boxWidth/2f);  // pMemo : given number
         pMemo.setColor(Color.BLACK);
-        int xGap = boxWidth/3;
-        int yGap = boxWidth*2/3;
         for (int nbrInPage = 0; nbrInPage < answerTables.length; nbrInPage++) {
             int [][] ansTable = str2suArray(answerTables[nbrInPage]);
             int [][] blankTable = str2suArray(blankTables[nbrInPage]);
             int xBase = space +20 + (nbrInPage % 4) * boxWidth * 95 / 10;
             int yBase = space + (nbrInPage / 4) * boxWidth * 10 + boxWidth + 40;
+            int xGap = boxWidth/2;
+            int yGap = boxWidth*3/4;
             for (int row = 0; row < 9; row++) {
                 for (int col = 0; col < 9; col++) {
                     int xPos = xBase + col * boxWidth;
@@ -174,7 +174,7 @@ class MakePDF {
                             (blankTable[row][col] == 0)? pNumb:pMemo);
                 }
             }
-            canvas.drawText("[ "+nbrInPage+" ]", xBase + 60, yBase - 8, pNumb);
+            canvas.drawText("["+nbrInPage+"]", xBase + 60, yBase - 8, pMemo);
 
             for (int row = 0; row < 9; row+=3)
                 for (int col = 0; col < 9; col+=3) {
