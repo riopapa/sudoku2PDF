6612f7a59699ef4fc398438a516d9d635581bc2e
Woncherl Ha (하원철) <riopapa@gmail.com>
2023-02-27 17:27:37 +0900

    Refine Signature info, page counts, model separate

---
+++ app/src/main/java/com/urrecliner/sudoku2pdf/Model/SudokuInfo.java
@@ -0,0 +1,10 @@
+package com.urrecliner.sudoku2pdf.Model;
+
+import android.content.Context;
+
+public class SudokuInfo {
+    public Context context;
+    public String dateTime;
+    public int pageCount;
+    public int blankCount;
+}
