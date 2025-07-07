3777596e1f939d4aa90c1d2656beb5bd0d48d651
Woncherl Ha (하원철) <riopapa@gmail.com>
2025-07-07 21:20:02 +0900

    starting 6x6
    Signed-off-by: Woncherl Ha (하원철) <riopapa@gmail.com>

---
+++ app/build/generated/source/buildConfig/androidTest/debug/com/riopapa/sudoku2pdf/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.riopapa.sudoku2pdf.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.riopapa.sudoku2pdf.test";
+  public static final String BUILD_TYPE = "debug";
+}
